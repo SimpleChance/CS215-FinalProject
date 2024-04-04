27e27a1686e880de2303023b1e7da9407d940d07
SimpleChance <cwjewell19@gmail.com>
2024-04-03 22:38:57 -0400

    Temporarily fixed scaling issue. Added fonts and draw_text() to display data on the screen. Updated draw_frame() to include new text and optimum path.

--- Travelling Salesman/render.py
+++ Travelling Salesman/render.py
@@ -31,6 +31,8 @@ class Renderer(object):
             self.screen = pg.display.set_mode(display_dimensions)
 
         self.clock = pg.time.Clock()
+        self.header_font = pg.font.SysFont('Comic Sans MS', 20)
+        self.text_font = pg.font.SysFont('Comic Sans MS', 15)
         self.max_framerate = max_framerate
         self.display_dimensions = display_dimensions
 
@@ -38,7 +40,9 @@ class Renderer(object):
         self.dimensions = dimensions
         self.node_to_display_scale_x = (display_dimensions[0] / dimensions[0]) * scale
         self.node_to_display_scale_y = (display_dimensions[1] / dimensions[1]) * scale
-        self.display_offset = [0, 0]
+        self.display_offset = [(dimensions[0] * self.node_to_display_scale_x) // 2,
+                               (dimensions[1] * self.node_to_display_scale_y) // 2]
+        self.node_space_offset = [0, -100]
 
         self.node_radius = node_radius
         self.path_width = path_width
@@ -65,19 +69,21 @@ class Renderer(object):
         pg.display.flip()
 
     def conv_to_render_coords(self, node):
-        return [(node[0] * self.node_to_display_scale_x) + self.display_offset[0],
-                (node[1] * self.node_to_display_scale_y) + self.display_offset[1]]
+        return [(node[0] * self.node_to_display_scale_x) + self.display_offset[0] + self.node_space_offset[0],
+                (node[1] * self.node_to_display_scale_y) + self.display_offset[1] + self.node_space_offset[1]]
 
     def draw_background(self):
         self.screen.fill(self.background_color)
 
     def draw_node_space_border(self):
         x, y = self.conv_to_render_coords([0, 0])
-        x -= self.node_radius*2
-        y -= self.node_radius*2
-        l, w = self.conv_to_render_coords([self.dimensions[0] * self.scale, self.dimensions[1] * self.scale])
-        l += self.node_radius*4
-        w += self.node_radius*4
+        x -= self.node_radius*4
+        y -= self.node_radius*4
+        l, w = self.conv_to_render_coords([self.dimensions[0], self.dimensions[1]])
+        l -= (self.display_offset[0] + self.node_space_offset[0])
+        w -= (self.display_offset[1] + self.node_space_offset[1])
+        l += self.node_radius*8
+        w += self.node_radius*8
         pg.draw.rect(self.screen, self.border_color, (x, y, l, w), width=self.border_width)
 
     def draw_nodes(self, coords):
@@ -98,9 +104,25 @@ class Renderer(object):
             end_pos = self.conv_to_render_coords(coords[path[i+1]])
             pg.draw.line(self.screen, color, start_pos, end_pos, self.path_width)
 
-    def draw_frame(self, coords, path):
+    def draw_header(self, header, pos):
+        header_surface = self.header_font.render(header, False, (0, 0, 0))
+        self.screen.blit(header_surface, pos)
+
+    def draw_text(self, text, pos):
+        text_surface = self.text_font.render(text, False, (0, 0, 0))
+        self.screen.blit(text_surface, pos)
+
+    def draw_frame(self, coords, path, text, opt_path=None):
         self.draw_background()
         self.draw_node_space_border()
         self.draw_nodes(coords)
-        self.draw_path(path, coords)
+        if opt_path:
+            self.draw_path(opt_path, coords, (255, 200, 200))
+            self.draw_path(path, coords)
+        else:
+            self.draw_path(path, coords)
+
+        self.draw_header(f"Best Fit:",
+                         [self.display_dimensions[0]//5 + 15, 3*self.display_dimensions[1]//5 + 10])
+        self.draw_text(text[1], (self.display_dimensions[1]//5 + 45, 3*self.display_dimensions[1]//5 + 35))
         self.update()
