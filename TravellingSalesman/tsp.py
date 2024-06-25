be2d9d6fd3d5b036878e44ec7633cc2ab9692557
SimpleChance <cwjewell19@gmail.com>
2024-06-25 16:10:23 -0400

    Updated TSP Instance Parser

--- TravellingSalesman/tsp.py
+++ TravellingSalesman/tsp.py
@@ -2,45 +2,99 @@
 File for TSP related methods
 """
 import numpy as np
+import os
 
 
-def parse_instance(text):
-    """
-        Description: Parses a .tsp instance for relevant information.
-        Args: list[string]
-        Returns: int, list[int], list[list[int]], list[int]
-    """
-    nodes = []
-    coords = []
-    dimensions = [0, 0]
-
-    num_nodes = int(text[3][11::])
-    for i in range(num_nodes):
-        tmp = text[6+i].split(sep=' ')
-        nodes.append(int(tmp[0]))
-        tmp_x, tmp_y = int(float(tmp[1])), int(float(tmp[2]))
-        coords.append([tmp_x, tmp_y])
-        if tmp_x > dimensions[0]:
-            dimensions[0] = tmp_x
-        if tmp_y > dimensions[1]:
-            dimensions[1] = tmp_y
-
-    return num_nodes, nodes, coords, dimensions
+class TSPInstance:
+    def __init__(self, name, nodes, coords, dimensions, best_path=None):
+        self.name = name
+        self.nodes = nodes
+        self.coords = coords
+        self.dimensions = dimensions
+        self.best_path = best_path
 
 
-def parse_opt_tour(text):
-    """
-        Description: Parses a .opt.tsp instance into a list of nodes.
-        Args: list[string]
-        Returns: list[int]
-    """
-    opt_tour = []
-    num_nodes = int(text[2].split(sep=' ')[2])
-    for i in range(num_nodes):
-        tmp = text[4 + i]
-        opt_tour.append(int(tmp) - 1)
-    opt_tour.append(0)
-    return opt_tour
+class TSPParser:
+    def __init__(self):
+        self.directory = os.path.join(os.path.dirname(__file__), 'TSP Instances')
+        self.instances = []
+        self.parse_instances()
+
+    def parse_instances(self):
+        for filename in os.listdir(self.directory):
+            if filename.endswith(".tsp"):
+                tsp_file = os.path.join(self.directory, filename)
+                opt_tour_file = os.path.join(self.directory, filename.replace(".tsp", ".opt.tour"))
+                instance_data = self.parse_files(tsp_file, opt_tour_file)
+                self.instances.append(instance_data)
+
+    def parse_files(self, tsp_file, opt_tour_file):
+        # Parse .tsp file
+        name = os.path.basename(tsp_file)
+        name = os.path.splitext(name)[0]  # Remove .tsp extension
+        nodes, coords, dimensions = self.parse_tsp_file(tsp_file)
+
+        # Parse .opt.tour file if it exists
+        best_path = None
+        if os.path.exists(opt_tour_file):
+            best_path = self.parse_opt_tour_file(opt_tour_file)
+
+        return TSPInstance(name, nodes, coords, dimensions, best_path)
+
+    def parse_tsp_file(self, tsp_file):
+        nodes = []
+        coords = []
+
+        with open(tsp_file, 'r') as file:
+            lines = file.readlines()
+
+        in_node_section = False
+        for line in lines:
+            line = line.strip()
+            if line.startswith("NODE_COORD_SECTION"):
+                in_node_section = True
+            elif line.startswith("EOF"):
+                break
+
+            if in_node_section:
+                parts = line.split()
+                if len(parts) > 1:
+                    try:
+                        node_id = int(parts[0])
+                        x = float(parts[1])
+                        y = float(parts[2])
+                        nodes.append(node_id)
+                        coords.append((x, y))
+                    except ValueError:
+                        continue  # Skip lines that are not in expected format
+
+        dimensions = self.calculate_dimensions(coords)
+        return nodes, coords, dimensions
+
+    @staticmethod
+    def calculate_dimensions(coords):
+        min_x = min(coord[0] for coord in coords)
+        max_x = max(coord[0] for coord in coords)
+        min_y = min(coord[1] for coord in coords)
+        max_y = max(coord[1] for coord in coords)
+        dimensions = (max_x - min_x, max_y - min_y)
+        return dimensions
+
+    @staticmethod
+    def parse_opt_tour_file(opt_tour_file):
+        with open(opt_tour_file, 'r') as file:
+            lines = file.readlines()
+
+        best_path = []
+        for line in lines:
+            line = line.strip()
+            if line.isdigit():
+                best_path.append(int(line))
+
+        return best_path
+
+    def get_instances(self):
+        return self.instances
 
 
 def generate_random_coords(dimensions, num_nodes):
